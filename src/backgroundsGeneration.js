2e12a9b3ec3a21803416a70afa02b357b040aeb7
Egor <ieghor-popov-2000@mail.ru>
2019-02-03 00:08:35 +0300

    feat: delete files in master

--- src/backgroundsGeneration.js
+++
@@ -1,4 +0,0 @@
-const backgrounds = ["img/backgrounds/BGLAYERS_EXAMPLE[1].png", "img/backgrounds/desert_day[1].jpg", "img/backgrounds/terrain.png","img/backgrounds/fon14[1].png"];
-export default function randomBackground() {
-    return backgrounds[Math.floor(Math.random() * backgrounds.length)];
-}
--- src/spellBookButtonRender.js
+++
@@ -1,8 +0,0 @@
-export default function spellBookButtonRender(spellBook) {
-    spellBook.innerText = "Spellbook";
-    spellBook.style.marginLeft = "58.750em";
-    spellBook.style.marginTop = '8.125em';
-    spellBook.style.position = "absolute";
-    spellBook.style.zIndex = "1010";
-    spellBook.id = 'spellbutton';
-}
